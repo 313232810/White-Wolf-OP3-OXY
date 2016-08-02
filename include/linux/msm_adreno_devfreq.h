15a7e9f9f5190ec187be7d4c862d47ac1f5e5625
tbalden <illespal@gmail.com>
2016-08-02 14:37:31 -0700

    gpu: devfreq: adrenoboost: adding conservative governor
    Adding a governor where moving upwards at lowest levels
    and highest levels is hard (defending 2d and overboost),
    and easy to change freq level in the middle part of the
    frequency levels.

--- include/linux/msm_adreno_devfreq.h
+++ include/linux/msm_adreno_devfreq.h
@@ -29,9 +29,8 @@ struct devfreq_msm_adreno_tz_data {
 		s64 total_time;
 		s64 busy_time;
 #if 1
-		int cycles_without_boost;
-		int boost_period;
 		int last_level;
+		int cycles_keeping_level;
 #endif
 		u32 ctxt_aware_target_pwrlevel;
 		u32 ctxt_aware_busy_penalty;
